12f91629f31b314c40781020fffd98bd1f7a317d
Bishow B <bishowb@gmail.com>
2025-10-14 20:20:53 +0200

    Add Plausible analytics script for privacy-friendly tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./prism-theme.css"
 import { Inter } from "next/font/google"
 import { ThemeProvider } from "@/components/theme-provider"
 import Header from "@/components/header"
+import  Script  from "next/script"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -20,6 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Privacy-friendly analytics by Plausible */}
+        <Script async src="https://plausible.io/js/pa-s-ef3BSOItwjLT6cueUqs.js"></Script>
+        <Script dangerouslySetInnerHTML={{ __html: `window.plausible=window.plausible||function(){(plausible.q=plausible.q||[]).push(arguments)},plausible.init=plausible.init||function(i){plausible.o=i||{}};plausible.init()` }} />
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           <Header />
